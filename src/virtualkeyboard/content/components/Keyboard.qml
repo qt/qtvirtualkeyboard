qt/qtvirtualkeyboard

commit 2b94afe874676a5ed173573d8e313ed202947ead

Improve input mode handling
Replaced the generic binding for inputMethodHints with
a specific binding to ImhPreferNumbers hint.

The problem with the generic binding was that it occurs
at point which the input mode has already changed, thus
causes an extra change to the input mode, if the numbers
hint is set or reset.

This may be a problem for the specific input methods, which
may load different data model for different input modes
(e.g. handwriting recognition).

Simplified the input mode selection in updateInputMethod()
function. The order and preference which input mode is used
is more clear now.

Note: Updated the Japanese tests because the input mode
is preserved when the focus is removed and added to same
control. Previously the tests rely on the fact that the
input mode was reset to default when the focus was reset.

Change-Id: I3196919ab4151a629eaa92cbedb21b7d03591051
Reviewed-by: Mitch Curtis <mitch.curtis@theqtcompany.com>

src/virtualkeyboard/content/components/Keyboard.qml
@@ -36,6 +36,8 @@ Item {
     property string inputLocale
     property int defaultLocaleIndex: -1
     property string defaultLocale: defaultLocaleIndex >= 0 && defaultLocaleIndex < layoutsModel.count ? layoutsModel.get(defaultLocaleIndex, "fileName") : ""
+    property bool latinOnly: InputContext.inputMethodHints & Qt.ImhLatinOnly
+    property bool preferNumbers: InputContext.inputMethodHints & Qt.ImhPreferNumbers
     property string layout
     property string layoutType: {
         if (InputContext.inputMethodHints & Qt.ImhDialableCharactersOnly) return "dialpad"
@@ -46,7 +48,6 @@ Item {
     }
     property bool active: Qt.inputMethod.visible
     property bool uppercased: InputContext.shift
-    property bool latinOnly: InputContext.inputMethodHints & Qt.ImhLatinOnly
     property bool symbolMode
     property var defaultInputMethod: initDefaultInputMethod()
     property var plainInputMethod: PlainInputMethod {}
@@ -83,19 +84,22 @@ Item {
         updateLayout()
     }
     onLatinOnlyChanged: {
+        if (!latinOnly)
+            inputModeNeedsReset = true
+        updateInputMethod()
+    }
+    onPreferNumbersChanged: {
+        keyboard.symbolMode = preferNumbers
+        if (!preferNumbers)
+            inputModeNeedsReset = true
         updateInputMethod()
     }
 
     Connections {
         target: InputContext
-        onInputMethodHintsChanged: {
-            keyboard.symbolMode = InputContext.inputMethodHints & Qt.ImhPreferNumbers
-        }
         onFocusChanged: {
-            if (InputContext.focus) {
-                inputModeNeedsReset = true
+            if (InputContext.focus)
                 updateInputMethod()
-            }
         }
         onNavigationKeyPressed: {
             var initialKey
@@ -840,11 +844,6 @@ Item {
         if (!inputMethod)
             inputMethod = customInputMethod ? customInputMethod : defaultInputMethod
 
-        if (latinOnly)
-            inputMode = InputEngine.Latin
-        else if (keyboardLayoutLoader.item.inputMode !== -1)
-            inputMode = keyboardLayoutLoader.item.inputMode
-
         var inputMethodChanged = InputContext.inputEngine.inputMethod !== inputMethod
         if (inputMethodChanged) {
             InputContext.inputEngine.inputMethod = inputMethod
@@ -854,12 +853,26 @@ Item {
             var inputModes = InputContext.inputEngine.inputModes
             if (inputModes.length > 0) {
                 // Reset to default input mode if the input locale has changed
-                if ((!latinOnly && inputModeNeedsReset) || inputModes.indexOf(inputMode) === -1) {
+                if (inputModeNeedsReset)
                     inputMode = inputModes[0]
-                }
-                if (InputContext.inputEngine.inputMode !== inputMode || inputMethodChanged || inputModeNeedsReset) {
+
+                // Check the current layout for input mode override
+                if (keyboardLayoutLoader.item.inputMode !== -1)
+                    inputMode = keyboardLayoutLoader.item.inputMode
+
+                // Check the input method hints for input mode overrides
+                if (latinOnly)
+                    inputMode = InputEngine.Latin
+                if (preferNumbers)
+                    inputMode = InputEngine.Numeric
+
+                // Make sure the input mode is supported by the current input method
+                if (inputModes.indexOf(inputMode) === -1)
+                    inputMode = inputModes[0]
+
+                if (InputContext.inputEngine.inputMode !== inputMode || inputMethodChanged)
                     InputContext.inputEngine.inputMode = inputMode
-                }
+
                 inputModeNeedsReset = false
             }
         }

tests/auto/inputpanel/data/tst_inputpanel.qml
@@ -74,6 +74,8 @@ Rectangle {
             if (!inputPanel.isLocaleSupported(locale))
                 expectFail("", "Input locale not enabled")
             verify(inputPanel.setLocale(locale))
+            if (data !== undefined && data.hasOwnProperty("initInputMode"))
+                verify(inputPanel.setInputMode(inputPanel.mapInputMode(data.initInputMode)))
             Qt.inputMethod.show()
             waitForRendering(inputPanel)
             verify(inputPanel.visible === true)
@@ -705,11 +707,11 @@ Rectangle {
         function test_japaneseInputModes_data() {
             return [
                 // Hiragana
-                { initLocale: "ja_JP", inputSequence: ["n","i","h","o","n","g","o",Qt.Key_Return], outputText: "\u306B\u307B\u3093\u3054" },
+                { initLocale: "ja_JP", initInputMode: "Hiragana", inputSequence: ["n","i","h","o","n","g","o",Qt.Key_Return], outputText: "\u306B\u307B\u3093\u3054" },
                 // Hiragana to Kanjie conversion
-                { initLocale: "ja_JP", inputSequence: ["n","i","h","o","n","g","o",Qt.Key_Space,Qt.Key_Return], outputText: "\u65E5\u672C\u8A9E" },
+                { initLocale: "ja_JP", initInputMode: "Hiragana", inputSequence: ["n","i","h","o","n","g","o",Qt.Key_Space,Qt.Key_Return], outputText: "\u65E5\u672C\u8A9E" },
                 // Correction to Hiragana sequence using exact match mode
-                { initLocale: "ja_JP", inputSequence: [
+                { initLocale: "ja_JP", initInputMode: "Hiragana", inputSequence: [
                                 // Write part of the text leaving out "ni" from the beginning
                                 "h","o","n","g","o",
                                 // Activate the exact mode and move the cursor to beginning
@@ -723,9 +725,9 @@ Rectangle {
                                 // Choose the first candidate
                                 Qt.Key_Return], outputText: "\u65E5\u672C\u8A9E" },
                 // Katakana
-                { initLocale: "ja_JP", inputSequence: [Qt.Key_Mode_switch,"a","m","e","r","i","k","a"], outputText: "\u30A2\u30E1\u30EA\u30AB" },
+                { initLocale: "ja_JP", initInputMode: "Katakana", inputSequence: ["a","m","e","r","i","k","a"], outputText: "\u30A2\u30E1\u30EA\u30AB" },
                 // Toggle symbol mode without affecting the input method state
-                { initLocale: "ja_JP", inputSequence: ["n","i","h","o","n","g","o",Qt.Key_Context1], outputText: "" },
+                { initLocale: "ja_JP", initInputMode: "Hiragana", inputSequence: ["n","i","h","o","n","g","o",Qt.Key_Context1], outputText: "" },
                 // Latin only
                 { initLocale: "ja_JP", initInputMethodHints: Qt.ImhLatinOnly, inputSequence: "hello", outputText: "Hello" },
             ]

tests/auto/inputpanel/data/inputpanel/inputpanel.qml
@@ -33,6 +33,7 @@ InputPanel {
     property var virtualKeyPressPoint: null
     readonly property bool autoCapitalizationEnabled: InputContext.shiftHandler.autoCapitalizationEnabled
     readonly property bool toggleShiftEnabled: InputContext.shiftHandler.toggleShiftEnabled
+    readonly property int inputMode: InputContext.inputEngine.inputMode
     readonly property var keyboard: findChildByProperty(inputPanel, "objectName", "keyboard", null)
     readonly property var keyboardLayoutLoader: findChildByProperty(keyboard, "objectName", "keyboardLayoutLoader", null)
     readonly property var keyboardInputArea: findChildByProperty(keyboard, "objectName", "keyboardInputArea", null)
@@ -149,6 +150,35 @@ InputPanel {
             }
         }
         return success
+    }
+
+    function mapInputMode(inputModeName) {
+        if (inputModeName === "Latin")
+            return InputEngine.Latin
+        else if (inputModeName === "Numeric")
+            return InputEngine.Numeric
+        else if (inputModeName === "Dialable")
+            return InputEngine.Dialable
+        else if (inputModeName === "Pinyin")
+            return InputEngine.Pinyin
+        else if (inputModeName === "Hangul")
+            return InputEngine.Hangul
+        else if (inputModeName === "Hiragana")
+            return InputEngine.Hiragana
+        else if (inputModeName === "Katakana")
+            return InputEngine.Katakana
+        else if (inputModeName === "FullwidthLatin")
+            return InputEngine.FullwidthLatin
+        else
+            return -1
+    }
+
+    function setInputMode(inputMode) {
+        if (InputContext.inputEngine.inputModes.indexOf(inputMode) === -1)
+            return false
+        if (InputContext.inputEngine.inputMode !== inputMode)
+            InputContext.inputEngine.inputMode = inputMode
+        return true
     }
 
     function findVirtualKey(key) {

commit b22e3fab1fc028518633bbaab10e40d5920be773

Fix regressions introduced in commit 2b94afe8
In the Keyboard.qml the input mode should always be set when the
input locale changes.

In the hunspell input method, the dictionary should be loaded even
if the prediction is not allowed by the input method hints. It was
originally prevented because the test cases were polluted with
unnecessary warnings by the missing dictionary files. Now the
qWarning is replaced with a debug message and the dictionary is
loaded regardless of the current input method hints.

In the tests, there was an unnecessary Qt.Key_Context1 left in the
japanese test case.

Change-Id: I6442d0c9b485d5126c1bc8b6b7fcd0a240913f0f
Reviewed-by: Rainer Keller <rainer.keller@theqtcompany.com>

src/virtualkeyboard/content/components/Keyboard.qml
@@ -874,7 +874,7 @@ Item {
                 if (inputModes.indexOf(inputMode) === -1)
                     inputMode = inputModes[0]
 
-                if (InputContext.inputEngine.inputMode !== inputMode || inputMethodChanged)
+                if (InputContext.inputEngine.inputMode !== inputMode || inputMethodChanged || inputModeNeedsReset)
                     InputContext.inputEngine.inputMode = inputMode
 
                 inputModeNeedsReset = false

src/virtualkeyboard/hunspellinputmethod.cpp
@@ -89,7 +89,7 @@ public:
                 }
             }
             if (!hunspell) {
-                qWarning() << "Missing Hunspell dictionary for locale" << locale << "in" << searchPaths;
+                VIRTUALKEYBOARD_DEBUG() << "Missing Hunspell dictionary for locale" << locale << "in" << searchPaths;
                 this->locale.clear();
                 return false;
             }
@@ -207,8 +207,6 @@ bool HunspellInputMethod::setInputMode(const QString &locale, DeclarativeInputEn
 {
     Q_UNUSED(inputMode)
     Q_D(HunspellInputMethod);
-    if (inputContext()->inputMethodHints().testFlag(Qt::ImhNoPredictiveText))
-        return true;
     return d->createHunspell(locale);
 }
 

tests/auto/inputpanel/data/tst_inputpanel.qml
@@ -727,7 +727,7 @@ Rectangle {
                 // Katakana
                 { initLocale: "ja_JP", initInputMode: "Katakana", inputSequence: ["a","m","e","r","i","k","a"], outputText: "\u30A2\u30E1\u30EA\u30AB" },
                 // Toggle symbol mode without affecting the input method state
-                { initLocale: "ja_JP", initInputMode: "Hiragana", inputSequence: ["n","i","h","o","n","g","o",Qt.Key_Context1], outputText: "" },
+                { initLocale: "ja_JP", initInputMode: "Hiragana", inputSequence: ["n","i","h","o","n","g","o"], outputText: "" },
                 // Latin only
                 { initLocale: "ja_JP", initInputMethodHints: Qt.ImhLatinOnly, inputSequence: "hello", outputText: "Hello" },
             ]