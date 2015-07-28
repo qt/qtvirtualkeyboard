efb4423ef07193f62f101b5beb7bdb2c27485c92
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-07-28 14:54:51 +0200

    Make examples return -1 on load failures.
    Avoid hanging processes and empty windows in case some module is not
    installed.
    
    Change-Id: I54a96ba0da9220198cc9443a581d632be1d6b903
    Reviewed-by: Jarkko Koivikko <jarkko.koivikko@code-q.fi>

--- examples/quick/enterprise/virtualkeyboard/virtualkeyboard/main.cpp
+++ examples/quick/enterprise/virtualkeyboard/virtualkeyboard/main.cpp
@@ -26,6 +26,8 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
     QQuickView view(QString("qrc:/%2").arg(MAIN_QML));
+    if (view.status() == QQuickView::Error)
+        return -1;
     view.setResizeMode(QQuickView::SizeRootObjectToView);
 
     view.show();
