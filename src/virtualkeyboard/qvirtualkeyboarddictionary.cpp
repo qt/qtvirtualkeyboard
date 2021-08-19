c7dbfbf155e30cce645de0270f62f3872c5c058a
Jarkko Koivikko <jarkko.koivikko@code-q.fi>
2021-08-19 12:07:18 +0300

    Add missing documentation for the dictionary API
    Fixes: QTBUG-95893
    Change-Id: I4b3bd72caff39935fc85f19bb5f25d802f23abb3
    Reviewed-by: Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>
    (cherry picked from commit 4b25f031d8c6cc7da3dd82a4da7ccaaf60822667)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/virtualkeyboard/qvirtualkeyboarddictionary.cpp
+++ src/virtualkeyboard/qvirtualkeyboarddictionary.cpp
@@ -29,6 +29,18 @@
 
 #include <QtVirtualKeyboard/qvirtualkeyboarddictionary.h>
 
+/*!
+    \class QVirtualKeyboardDictionary
+
+    \inmodule QtVirtualKeyboard
+
+    \brief An application-defined dictionary for virtual keyboard input methods.
+
+    This class defines a named dictionary that consists of words or phrases. The
+    dictionary can be enabled in the desired context and its usage depends on
+    the current input method of the virtual keyboard.
+*/
+
 QVirtualKeyboardDictionary::QVirtualKeyboardDictionary(const QString &name, QObject *parent) :
     QObject(parent),
     _name(name)
@@ -53,6 +65,30 @@ void QVirtualKeyboardDictionary::setContents(const QStringList &wordList)
         emit contentsChanged();
     }
 }
+
+/*!
+    \property QVirtualKeyboardDictionary::name
+    \brief name of the dictionary.
+
+    This property holds the name of the dictionary that was specified when
+    the dictionary was created. The name is necessary when referring to a
+    specific dictionary (for example when you activate it in the input context).
+
+    \note The dictionary name cannot be changed later.
+*/
+
+/*!
+    \property QVirtualKeyboardDictionary::contents
+    \brief contents of the dictionary.
+
+    This property holds the contents of the dictionary. Typically, the content
+    consists of words or phrases. Note that the dictionary is language neutral,
+    meaning it is the application's responsibility to localize the dictionary if
+    it contains language-dependent data.
+
+    The content can be set at any time, although it makes sense to set it when
+    the input method is not active.
+*/
 
 QT_BEGIN_NAMESPACE
 
