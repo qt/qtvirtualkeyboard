29f614d24836880d8c974faadfbe81fbfdce5086
Lucie Gérard <lucie.gerard@qt.io>
2022-06-13 14:03:01 +0200

    Use SPDX license identifiers
    Replace the current license disclaimer in files by
    a SPDX-License-Identifier.
    License files are organized under LICENSES directory.
    
    Pick-to: 6.4
    Task-number: QTBUG-67283
    Change-Id: Iaeb3cb05cd781031a35e40f712805fa265116076
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>

--- tests/auto/inputpanel/hwr_test_data/build_unipen_data.py
+++ tests/auto/inputpanel/hwr_test_data/build_unipen_data.py
@@ -1,33 +1,7 @@
 #!/usr/bin/env python
 
-#############################################################################
-##
-## Copyright (C) 2017 The Qt Company Ltd.
-## Contact: https://www.qt.io/licensing/
-##
-## This file is part of the Qt Virtual Keyboard module of the Qt Toolkit.
-##
-## $QT_BEGIN_LICENSE:GPL$
-## Commercial License Usage
-## Licensees holding valid commercial Qt licenses may use this file in
-## accordance with the commercial license agreement provided with the
-## Software or, alternatively, in accordance with the terms contained in
-## a written agreement between you and The Qt Company. For licensing terms
-## and conditions see https://www.qt.io/terms-conditions. For further
-## information use the contact form at https://www.qt.io/contact-us.
-##
-## GNU General Public License Usage
-## Alternatively, this file may be used under the terms of the GNU
-## General Public License version 3 or (at your option) any later version
-## approved by the KDE Free Qt Foundation. The licenses are as published by
-## the Free Software Foundation and appearing in the file LICENSE.GPL3
-## included in the packaging of this file. Please review the following
-## information to ensure the GNU General Public License requirements will
-## be met: https://www.gnu.org/licenses/gpl-3.0.html.
-##
-## $QT_END_LICENSE$
-##
-#############################################################################
+# Copyright (C) 2017 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 import os
 import sys
@@ -39,34 +13,10 @@ import re
 unipen_file_pattern = re.compile(r'(^[0-9]{2,9}).*\.txt')
 
 def print_header():
-    print """/****************************************************************************
-**
-** Copyright (C) %s The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the Qt Virtual Keyboard module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:GPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 or (at your option) any later version
-** approved by the KDE Free Qt Foundation. The licenses are as published by
-** the Free Software Foundation and appearing in the file LICENSE.GPL3
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/""" % datetime.datetime.now().year
+    print """
+// Copyright (C) %s The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+""" % datetime.datetime.now().year
 
 def scan_unipen_files(path):
     file_list = []
