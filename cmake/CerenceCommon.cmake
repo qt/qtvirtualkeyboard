ffc2c28a7459b89caa82902ee07d5d8d1233424a
Lucie Gérard <lucie.gerard@qt.io>
2022-08-22 16:11:15 +0200

    Change the license of all CMakeLists.txt and *.cmake files to BSD
    Task-number: QTBUG-105718
    Change-Id: I910246fa10b5be2742c9e9840e37b16c908c70a6
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>

--- cmake/CerenceCommon.cmake
+++ cmake/CerenceCommon.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+# SPDX-License-Identifier: BSD-3-Clause
 
 if(NOT CERENCE_SDK_ROOT)
     if(INPUT_vkb_cerence_sdk)
