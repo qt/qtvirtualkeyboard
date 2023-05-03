16b7025874a83ae1f39fac9339d368b34eebedff
Jarkko Koivikko <jarkko.koivikko@code-q.fi>
2023-05-03 15:15:37 +0300

    Fix VirtualKeyboardSettings.fullScreenMode binding
    The VirtualKeyboardSettings.fullScreenMode behavior was broken in commit
    e9c701803d because the change did not apply initial value.
    
    Fix the issue by initializing the shadow input control at component
    completed handler. Also, clear the contents of shadow input control
    when shadow input is not active. Add new test case to verify.
    
    Fixes: QTBUG-113168
    Change-Id: I3bbe44229bcae7e85ae253dbcd30d500326bc4da
    Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>
    (cherry picked from commit d902889159ed9ce429d9020d91701f1f9f0206e1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ tests/auto/shadowinput/tst_shadowinput.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <QtQuickTest/quicktest.h>
+#include <QByteArray>
+#include <QStandardPaths>
+#include <QFileInfo>
+#include <QDir>
+
+static bool s_configEnv = qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
+QUICK_TEST_MAIN(shadowinput)
