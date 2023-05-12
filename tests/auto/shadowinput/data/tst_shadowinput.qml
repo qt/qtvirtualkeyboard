qt/qtvirtualkeyboard

commit e9c701803dffe52fd9ac65c8657950213d948ebe

Fix full screen input control sync (disable when not active)
The full screen input (aka shadow input) was continued being
synchronized even after the full screen mode was disabled. Not only
this caused an unnecessary overhead, it resulted in incorrect behavior
in some cases when initializing cursorPosition and selection.

This caused some of the test to fail without obvious reason. The root
cause was the cursor synchronization of the shadow input setting the
cursor during the tests. Now the shadow input item is reset when full
screen mode is disabled. In addition, the cursor synchronization is
not run when full screen mode is toggled.

Remove affected test cases from the blacklist.

Fixes: QTBUG-97901
Task-number: QTBUG-97830
Pick-to: 6.5 6.4
Change-Id: I571433adfdb5a3f3cd9d994f0d6e916cae372054
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Inho Lee <inho.lee@qt.io>

src/components/ShadowInputControl.qml
@@ -106,11 +106,11 @@ Item {
         }
     }
 
-    Binding {
-        target: InputContext.priv.shadow
-        property: "inputItem"
-        value: shadowInput
-        when: VirtualKeyboardSettings.fullScreenMode
-        restoreMode: Binding.RestoreBinding
+    Connections {
+        target: VirtualKeyboardSettings
+        function onFullScreenModeChanged() {
+            InputContext.priv.shadow.inputItem = VirtualKeyboardSettings.fullScreenMode ? shadowInput : null
+            cursorSyncTimer.stop()
+        }
     }
 }

tests/auto/inputpanel/BLACKLIST
@@ -7,11 +7,3 @@
 *
 [tst_plugin::test_fullScreenModeSelectionHandles:row 3]
 *
-
-# QTBUG-97830
-[tst_plugin::test_selection:row 0]
-*
-
-# QTBUG-97901
-[tst_plugin::test_hangulInputMethod:row 24]
-*

commit d902889159ed9ce429d9020d91701f1f9f0206e1

Fix VirtualKeyboardSettings.fullScreenMode binding
The VirtualKeyboardSettings.fullScreenMode behavior was broken in commit
e9c701803d because the change did not apply initial value.

Fix the issue by initializing the shadow input control at component
completed handler. Also, clear the contents of shadow input control
when shadow input is not active. Add new test case to verify.

Fixes: QTBUG-113168
Pick-to: 6.5
Change-Id: I3bbe44229bcae7e85ae253dbcd30d500326bc4da
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/components/ShadowInputControl.qml
@@ -9,6 +9,7 @@ import QtQuick.VirtualKeyboard.Settings
 
 Item {
     id: control
+    property alias textEdit: shadowInput
 
     enabled: keyboard.active && VirtualKeyboardSettings.fullScreenMode
 
@@ -106,11 +107,23 @@ Item {
         }
     }
 
+    Component.onCompleted: {
+        if (VirtualKeyboardSettings.fullScreenMode) {
+            InputContext.priv.shadow.inputItem = shadowInput
+        }
+    }
     Connections {
         target: VirtualKeyboardSettings
         function onFullScreenModeChanged() {
             InputContext.priv.shadow.inputItem = VirtualKeyboardSettings.fullScreenMode ? shadowInput : null
+        }
+    }
+    Connections {
+        target: InputContext.priv.shadow
+        function onInputItemChanged() {
             cursorSyncTimer.stop()
+            if (!InputContext.priv.shadow.inputItem)
+                shadowInput.clear()
         }
     }
 }

tests/auto/CMakeLists.txt
@@ -12,3 +12,4 @@ add_subdirectory(layoutfilesystem)
 add_subdirectory(layoutresources)
 # add_subdirectory(cmake) # special case
 add_subdirectory(dictionarymanager)
+add_subdirectory(shadowinput)

tests/auto/shadowinput/CMakeLists.txt
@@ -1,0 +1,17 @@
+# Copyright (C) 2023 The Qt Company Ltd.
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Collect test data
+file(GLOB_RECURSE test_data_glob
+    RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}
+    ${CMAKE_CURRENT_SOURCE_DIR}/data/*)
+list(APPEND test_data ${test_data_glob})
+
+qt_internal_add_test(tst_shadowinput
+    QMLTEST
+    SOURCES
+        tst_shadowinput.cpp
+    LIBRARIES
+        Qt::Gui
+    TESTDATA ${test_data}
+)

tests/auto/shadowinput/data/inputpanel/inputpanel.qml
@@ -1,0 +1,24 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+import QtTest
+import QtQuick
+import QtQuick.VirtualKeyboard
+import QtQuick.VirtualKeyboard.Settings
+
+InputPanel {
+    id: inputPanel
+    z: 99
+    anchors.left: parent.left
+    anchors.right: parent.right
+    anchors.bottom: parent.bottom
+    visible: active
+    property bool fullScreenMode: true
+    property bool shadowInputVisible: keyboard.shadowInputControl.visible
+    property string shadowInputText: keyboard.shadowInputControl.textEdit.text
+    Binding {
+        target: VirtualKeyboardSettings
+        property: "fullScreenMode"
+        value: inputPanel.fullScreenMode
+    }
+}

tests/auto/shadowinput/data/tst_shadowinput.qml
@@ -1,0 +1,92 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+import QtTest
+import QtQuick
+import QtQuick.Window
+
+Rectangle {
+    id: container
+    width: 800
+    height: 640
+    color: "blue"
+
+    Component {
+        id: textInputComp
+        TextEdit {
+            anchors.fill: parent
+            visible: true
+            focus: true
+            color: "white"
+        }
+    }
+
+    TestCase {
+        id: testcase
+        name: "tst_inputpanelcontrols"
+        when: windowShown
+
+        property var inputPanel: null
+        property var textInput: null
+
+        function initTestCase() {
+            var inputPanelComp = Qt.createComponent("inputpanel/inputpanel.qml")
+            compare(inputPanelComp.status, Component.Ready, "Failed to create component: "+inputPanelComp.errorString())
+            inputPanel = inputPanelComp.createObject(container)
+
+            textInput = textEditComp.createObject(container)
+        }
+
+        function cleanupTestCase() {
+            if (inputPanel)
+                inputPanel.destroy()
+        }
+
+        function prepareTest() {
+            var window = container.Window.window
+            verify(window)
+            window.raise()
+            window.requestActivate()
+            tryCompare(window, "active", true)
+
+            container.forceActiveFocus()
+            waitForRendering(container)
+            textInput.forceActiveFocus()
+            waitForRendering(inputPanel)
+
+            textInput.text = ""
+            verify(inputPanel.visible === true)
+            verify(textInput.activeFocus === true)
+        }
+
+        Component {
+            id: textEditComp
+            TextEdit {
+                id: textEdit
+                visible: true
+                focus: true
+                color: "white"
+            }
+        }
+
+        function test_fullScreenModeBindingWorks() {
+            prepareTest()
+
+            verify(inputPanel.visible === true)
+            waitForRendering(inputPanel)
+
+            // VirtualKeyboardSettings.fullScreeenMode is initially true
+            // Verity that shadow input is visible and the content match with the edit field
+            verify(inputPanel.shadowInputVisible)
+            textInput.text = "hello"
+            compare(inputPanel.shadowInputText, textInput.text)
+
+            // Disable fullScreenMode and verify
+            inputPanel.fullScreenMode = false
+            waitForRendering(inputPanel)
+            verify(!inputPanel.shadowInputVisible)
+            textInput.text = "world"
+            compare(inputPanel.shadowInputText, "")
+        }
+    }
+}

tests/auto/shadowinput/tst_shadowinput.cpp
@@ -1,0 +1,12 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <QtQuickTest/quicktest.h>
+#include <QByteArray>
+#include <QStandardPaths>
+#include <QFileInfo>
+#include <QDir>
+
+static bool s_configEnv = qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
+QUICK_TEST_MAIN(shadowinput)