qt/qtvirtualkeyboard

commit 8ceee9b6af24911eac7b7f9862a293133f953b1a

Use unsigned type for enum KeyboardFunctionKey
These changes were made based on feedback on Qt 6.6 api review.

- Use unsigned type for enum KeyboardFunctionKey (avoid QTBUG-113792)
- Use 0xffffffff as the value of AllFunctionKeys to enable BC
- Use scoped name for QtVirtualKeyboard.KeyboardFunctionKeys values

This change amends commits 4ef502b77eedf1ec1683535143c01c43f4abfaf2
and 71daa433bf630af56998d6d5a524c23bb4fd8b17.

Pick-to: 6.6
Change-Id: I4fe33bbff07505280ba5e937fccf38d3ebbb7b6e
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

src/components/Keyboard.qml
@@ -1818,7 +1818,9 @@ Item {
     }
 
     function isFunctionPopupListAvailable() {
-        return VirtualKeyboardSettings.visibleFunctionKeys !== QtVirtualKeyboard.AllFunctionKeys ||
+        const allFunctionKeys = QtVirtualKeyboard.KeyboardFunctionKeys.HideFunctionKey |
+                              QtVirtualKeyboard.KeyboardFunctionKeys.LanguageFunctionKey
+        return (VirtualKeyboardSettings.visibleFunctionKeys & QtVirtualKeyboard.KeyboardFunctionKeys.AllFunctionKeys) !== allFunctionKeys ||
                 isHandwritingAvailable()
     }
 }

src/components/ChangeLanguageKey.qml
@@ -49,5 +49,5 @@ BaseKey {
     keyPanelDelegate: keyboard.style ? keyboard.style.languageKeyPanel : undefined
     onClicked: keyboard.doKeyboardFunction(QtVirtualKeyboard.ChangeLanguage, customLayoutsOnly)
     enabled: keyboard.isKeyboardFunctionAvailable(QtVirtualKeyboard.ChangeLanguage, customLayoutsOnly)
-    visible: VirtualKeyboardSettings.visibleFunctionKeys & QtVirtualKeyboard.LanguageFunctionKey
+    visible: VirtualKeyboardSettings.visibleFunctionKeys & QtVirtualKeyboard.KeyboardFunctionKeys.LanguageFunctionKey
 }

src/components/HideKeyboardKey.qml
@@ -24,5 +24,5 @@ BaseKey {
     highlighted: true
     onClicked: keyboard.doKeyboardFunction(QtVirtualKeyboard.HideInputPanel)
     keyPanelDelegate: keyboard.style ? keyboard.style.hideKeyPanel : undefined
-    visible: VirtualKeyboardSettings.visibleFunctionKeys & QtVirtualKeyboard.HideFunctionKey
+    visible: VirtualKeyboardSettings.visibleFunctionKeys & QtVirtualKeyboard.KeyboardFunctionKeys.HideFunctionKey
 }

src/settings/qquickvirtualkeyboardsettings.cpp
@@ -549,13 +549,13 @@ void QQuickVirtualKeyboardSettings::resetStyle()
     The value can be combination of the following flags:
 
     \list
-        \li \c QtVirtualKeyboard.NoFunctionKeys All function keys are hidden
-        \li \c QtVirtualKeyboard.HideFunctionKey Hide function key is visible
-        \li \c QtVirtualKeyboard.LanguageFunctionKey Language function key is visible
-        \li \c QtVirtualKeyboard.AllFunctionKeys All function keys are visible
+        \li \c QtVirtualKeyboard.KeyboardFunctionKeys.NoFunctionKeys All function keys are hidden
+        \li \c QtVirtualKeyboard.KeyboardFunctionKeys.HideFunctionKey Hide function key is visible
+        \li \c QtVirtualKeyboard.KeyboardFunctionKeys.LanguageFunctionKey Language function key is visible
+        \li \c QtVirtualKeyboard.KeyboardFunctionKeys.AllFunctionKeys All function keys are visible
     \endlist
 
-    The default is \c QtVirtualKeyboard.AllFunctionKeys.
+    The default is \c QtVirtualKeyboard.KeyboardFunctionKeys.AllFunctionKeys.
 */
 
 /*!

src/virtualkeyboard/qvirtualkeyboardnamespace.h
@@ -41,11 +41,11 @@ enum class KeyboardFunction {
 };
 Q_ENUM_NS(KeyboardFunction)
 
-enum class KeyboardFunctionKey {
+enum class KeyboardFunctionKey : quint32 {
     NoFunctionKeys = 0,
     HideFunctionKey = 0x1,
     LanguageFunctionKey = 0x2,
-    AllFunctionKeys = HideFunctionKey | LanguageFunctionKey,
+    AllFunctionKeys = 0xffffffff,
 };
 Q_DECLARE_FLAGS(KeyboardFunctionKeys, KeyboardFunctionKey)
 Q_DECLARE_OPERATORS_FOR_FLAGS(KeyboardFunctionKeys)

tests/auto/inputpanel/data/inputpanel/inputpanel.qml
@@ -251,18 +251,18 @@ InputPanel {
 
     function mapKeyboardFunctionKey(functionKeyName) {
         if (functionKeyName === "NoFunctionKeys")
-            return QtVirtualKeyboard.NoFunctionKeys
+            return QtVirtualKeyboard.KeyboardFunctionKeys.NoFunctionKeys
         if (functionKeyName === "HideFunctionKey")
-            return QtVirtualKeyboard.HideFunctionKey
+            return QtVirtualKeyboard.KeyboardFunctionKeys.HideFunctionKey
         if (functionKeyName === "LanguageFunctionKey")
-            return QtVirtualKeyboard.LanguageFunctionKey
+            return QtVirtualKeyboard.KeyboardFunctionKeys.LanguageFunctionKey
         if (functionKeyName === "AllFunctionKeys")
-            return QtVirtualKeyboard.AllFunctionKeys
+            return QtVirtualKeyboard.KeyboardFunctionKeys.AllFunctionKeys
         testcase.fail("Invalid function key '%1'".arg(functionKeyName))
     }
 
     function setVisibleFunctionKeys(functionKeyNames) {
-        let functionKeys = QtVirtualKeyboard.NoFunctionKeys
+        let functionKeys = QtVirtualKeyboard.KeyboardFunctionKeys.NoFunctionKeys
         for (const functionKeyName of functionKeyNames) {
             functionKeys |= mapKeyboardFunctionKey(functionKeyName)
         }

commit 29560e2df8cb5715307c18da6d0fd8815b68b3f8

Fix Settings icon visible in the comma key when Settings not available
This change fixes regression introduced in the commit
8ceee9b6af24911eac7b7f9862a293133f953b1a

Pick-to: 6.6
Change-Id: I059215e767e5ada809feed09f9089e8485a7528c
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

src/components/Keyboard.qml
@@ -1820,7 +1820,7 @@ Item {
     function isFunctionPopupListAvailable() {
         const allFunctionKeys = QtVirtualKeyboard.KeyboardFunctionKeys.HideFunctionKey |
                               QtVirtualKeyboard.KeyboardFunctionKeys.LanguageFunctionKey
-        return (VirtualKeyboardSettings.visibleFunctionKeys & QtVirtualKeyboard.KeyboardFunctionKeys.AllFunctionKeys) !== allFunctionKeys ||
+        return (VirtualKeyboardSettings.visibleFunctionKeys & allFunctionKeys) !== allFunctionKeys ||
                 isHandwritingAvailable()
     }
 }