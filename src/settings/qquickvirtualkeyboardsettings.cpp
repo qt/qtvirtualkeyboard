03aa77b218be99a8489f09556fc97df3097215b8
Ulf Hermann <ulf.hermann@qt.io>
2025-02-07 13:51:55 +0100

    Revert "Work around QML creating separate singletons for each version"
    This reverts commit cda3432fc154fa5fc8893f3ddc75475691cd3d2e.
    
    Since commit 4cdf0643b442a57b0cf0dcef104e71c0196dba06 in qtdeclarative
    the QML engine is less wasteful with creating singleton instances. This
    workaround should not be needed anymore.
    
    Task-number: QTBUG-127557
    Change-Id: I25df642ec4a500b4715fdc4b5f136dc81ce4012f
    Reviewed-by: Jarkko Koivikko <jarkko.koivikko@code-q.fi>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 204213ec3cd8772eb43d6ab2202e3ad5f70ec31c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 0c185f4d6d4606bb39b87748b98d91ccd5e3a3e5)

--- src/settings/qquickvirtualkeyboardsettings.cpp
+++ src/settings/qquickvirtualkeyboardsettings.cpp
@@ -10,7 +10,6 @@
 #include <QDir>
 #include <QRegularExpression>
 #include <QtCore/private/qobject_p.h>
-#include <QtCore/qmutex.h>
 
 QT_BEGIN_NAMESPACE
 namespace QtVirtualKeyboard {
@@ -157,18 +156,10 @@ QQuickVirtualKeyboardSettings::QQuickVirtualKeyboardSettings(QQmlEngine *engine,
 
 /*!
     \internal
-    TODO: Remove this method when QML stops creating separate singleton instances for each version.
  */
-QQuickVirtualKeyboardSettings *QQuickVirtualKeyboardSettings::create(
-        QQmlEngine *qmlEngine, QJSEngine *)
+QQuickVirtualKeyboardSettings *QQuickVirtualKeyboardSettings::create(QQmlEngine *qmlEngine, QJSEngine *)
 {
-    static QMutex mutex;
-    static QHash<QQmlEngine *, QQuickVirtualKeyboardSettings *> instances;
-    QMutexLocker locker(&mutex);
-    QQuickVirtualKeyboardSettings *&instance = instances[qmlEngine];
-    if (instance == nullptr)
-        instance = new QQuickVirtualKeyboardSettings(qmlEngine);
-    return instance;
+    return new QQuickVirtualKeyboardSettings(qmlEngine);
 }
 
 /*!
