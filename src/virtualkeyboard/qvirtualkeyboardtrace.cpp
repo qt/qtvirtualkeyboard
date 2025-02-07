f5b8e024004158b9cac36776db92e46086ef26c9
Topi Reinio <topi.reinio@qt.io>
2025-02-07 08:30:11 +0000

    Doc: Fix incorrect QML property type
    QDoc now warns about C++ types in QML property documentation.
    This change fixes existing documentation to prevent warnings.
    
    Change-Id: I57ac1f294079b7fb53ade279ec4ebc62afbd8a89
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/virtualkeyboard/qvirtualkeyboardtrace.cpp
+++ src/virtualkeyboard/qvirtualkeyboardtrace.cpp
@@ -458,7 +458,7 @@ void QVirtualKeyboardTrace::timerEvent(QTimerEvent *event)
     whose \c canceled property set to true.
 */
 
-/*! \qmlproperty qreal Trace::opacity
+/*! \qmlproperty real Trace::opacity
     \since QtQuick.VirtualKeyboard 2.4
 
     This property defines how opaque the Trace is.
