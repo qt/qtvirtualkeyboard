9194888bca176be0858d80f668c38dabf72982ea
Friedemann Kleint <Friedemann.Kleint@qt.io>
2025-06-11 13:27:22 +0200

    Add plugin parameter handling
    Make it possible to pass parameters like for the QPA platform plugins
    appended to the QT_IM_MODULE variable followed by a colon, like
    "qtvirtualkeyboard:style=retro,wordCandidateListVisible".
    
    This saves the overhead of instantiating a QQmlEngine just to set
    parameters for QtWidgets applications.
    
    [ChangeLog] It is now possible to append plugin parameters to QT_IM_MODULE.
    
    Task-number: QTBUG-131374
    Change-Id: I3bfdbcbd83c5dd38fc23e1bd9d54e707bde243d6
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Jarkko Koivikko <jarkko.koivikko@code-q.fi>
    (cherry picked from commit 0a964cb3a7e94a8343b7bc7af958eac66635c3c1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugin/plugin.cpp
+++ src/plugin/plugin.cpp
@@ -3,6 +3,7 @@
 // Qt-Security score:significant reason:default
 
 #include "plugin.h"
+#include <private/settings_p.h>
 #include <QtVirtualKeyboard/private/platforminputcontext_p.h>
 #include <QLoggingCategory>
 #if defined(Q_OS_WIN)
@@ -10,33 +11,85 @@
 #endif
 #include <qpa/qplatforminputcontextfactory_p.h>
 
+#include <algorithm>
+
 QT_BEGIN_NAMESPACE
+
+using namespace Qt::StringLiterals;
 
 using namespace QtVirtualKeyboard;
 
 Q_LOGGING_CATEGORY(qlcVirtualKeyboard, "qt.virtualkeyboard")
 
-static const char pluginName[] = "qtvirtualkeyboard";
+static constexpr auto pluginName = "qtvirtualkeyboard"_L1;
 
 QStringList QVirtualKeyboardPlugin::keys() const
 {
-    return QStringList(QLatin1String(pluginName));
+    return QStringList(pluginName);
+}
+
+static bool evaluateParameter(QStringView param)
+{
+    if (param == "wordCandidateListVisible"_L1) {
+        Settings::instance()->setWclAlwaysVisible(true);
+        return true;
+    }
+    if (param == "wordCandidateListAutoCommitWord"_L1) {
+        Settings::instance()->setWclAutoCommitWord(true);
+        return true;
+    }
+    if (param == "fullScreen"_L1) {
+        Settings::instance()->setFullScreenMode(true);
+        return true;
+    }
+
+    const auto equalsPos = param.indexOf(u'=');
+    if (equalsPos == -1 || equalsPos == param.size() - 1)
+        return false;
+
+    const auto keyword = param.sliced(0, equalsPos);
+    const auto value = param.sliced(equalsPos + 1);
+    if (keyword == "style"_L1) {
+        Settings::instance()->setStyleName(value.toString());
+        return true;
+    }
+    if (keyword == "locale"_L1) {
+        Settings::instance()->setLocale(value.toString());
+        return true;
+    }
+
+    return false;
+}
+
+static bool vkbRequested(const QString &req)
+{
+    return req.startsWith(pluginName);
 }
 
 QPlatformInputContext *QVirtualKeyboardPlugin::create(const QString &system, const QStringList &paramList)
 {
-    Q_UNUSED(paramList);
-
-    if (!QPlatformInputContextFactory::requested().contains(QLatin1StringView(pluginName)))
+    const auto &requested = QPlatformInputContextFactory::requested();
+    if (std::none_of(requested.cbegin(), requested.cend(), vkbRequested))
         return nullptr;
 
-    if (system.compare(system, QLatin1String(pluginName), Qt::CaseInsensitive) != 0)
+    if (system.compare(pluginName, Qt::CaseInsensitive) != 0)
         return nullptr;
 
 #if defined(Q_OS_WIN)
     // QTBUG-93042
     ImmDisableIME(0);
 #endif
+
+    for (const auto &param : paramList) {
+        QStringView paramV{param};
+        const auto params = paramV.split(u',');
+        for (const auto &param : params) {
+            if (!evaluateParameter(param)) {
+                qCWarning(qlcVirtualKeyboard, "Invalid parameter \"%s\".",
+                          qPrintable(param.toString()));
+            }
+        }
+    }
 
     PlatformInputContext *platformInputContext = new PlatformInputContext();
 
