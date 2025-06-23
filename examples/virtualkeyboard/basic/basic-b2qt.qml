qt/qtvirtualkeyboard

commit 7ed096808746f0eb51fb5d7f275e6640130cf639

Fix basic examples startup warnings
Switch styling to basic because native types(Windows and macOS) are not
suitable for customization

Fixes: QTBUG-123415
Pick-to: 6.8
Change-Id: I54c5f1c1fa413c137333561b723180046ced3288
Reviewed-by: Jarkko Koivikko <jarkko.koivikko@code-q.fi>

examples/virtualkeyboard/basic/basic-b2qt.qml
@@ -62,8 +62,6 @@ Item {
             anchors { left: parent.left; top: parent.top; right: parent.right; bottom: inputPanel.top; }
             HandwritingModeButton {
                 id: handwritingModeButton
-                anchors.top: parent.top
-                anchors.right: parent.right
                 anchors.margins: 10
                 floating: true
                 flipable: true
@@ -72,6 +70,10 @@ Item {
                 state: handwritingInputPanel.state
                 onClicked: handwritingInputPanel.active = !handwritingInputPanel.active
                 onDoubleClicked: handwritingInputPanel.available = !handwritingInputPanel.available
+                Component.onCompleted: {
+                    anchors.top = parent.top;
+                    anchors.right = parent.right
+                }
             }
         }
 

examples/virtualkeyboard/basic/content/TextArea.qml
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 import QtQuick
-import QtQuick.Controls as Controls
+import QtQuick.Controls.Basic as Controls
 import QtQuick.VirtualKeyboard
 
 Controls.TextArea {

examples/virtualkeyboard/basic/content/TextField.qml
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 import QtQuick
-import QtQuick.Controls as Controls
+import QtQuick.Controls.Basic as Controls
 import QtQuick.VirtualKeyboard
 
 Controls.TextField {

commit d83105d010539ae634a3491ede0ca1f9c1b1e045

basic: Fix HandwritingModeButton size (regression at 7ed0968087)
HandwritingModeButton must be initially bounded to top/right anchors.
This binding was broken in commit 7ed0968087, however the bindings
are overridden on purpose in the HandwritingModeButton. The anchor
bindings are used on temporary basis to provide snapping functionality
of the HandwritingModeButton.

Pick-to: 6.8 6.9 6.10
Task-number: QTBUG-123415
Change-Id: I70944f3f8a8d7beac4d6fac7f0e05bd39c1e1655
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

examples/virtualkeyboard/basic/basic-b2qt.qml
@@ -62,6 +62,8 @@ Item {
             anchors { left: parent.left; top: parent.top; right: parent.right; bottom: inputPanel.top; }
             HandwritingModeButton {
                 id: handwritingModeButton
+                anchors.top: parent.top
+                anchors.right: parent.right
                 anchors.margins: 10
                 floating: true
                 flipable: true
@@ -70,10 +72,6 @@ Item {
                 state: handwritingInputPanel.state
                 onClicked: handwritingInputPanel.active = !handwritingInputPanel.active
                 onDoubleClicked: handwritingInputPanel.available = !handwritingInputPanel.available
-                Component.onCompleted: {
-                    anchors.top = parent.top;
-                    anchors.right = parent.right
-                }
             }
         }
 